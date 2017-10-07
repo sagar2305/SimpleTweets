8dd836f49343734eb9ab388bb746878d88b22a01
Sagar Mutha <sagar2305@gmail.com>
2017-10-07 14:58:16 -0700

    add screen name to title bar

--- app/src/main/java/com/codepath/apps/restclienttemplate/activities/ProfileActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/activities/ProfileActivity.java
@@ -6,8 +6,19 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.codepath.apps.restclienttemplate.R;
 import com.codepath.apps.restclienttemplate.fragments.UserTimelineFragment;
+import com.codepath.apps.restclienttemplate.models.User;
+import com.codepath.apps.restclienttemplate.utils.TwitterApp;
+import com.codepath.apps.restclienttemplate.utils.TwitterClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    TwitterClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +37,19 @@ public class ProfileActivity extends AppCompatActivity {
 
         // commit transaction
         ft.commit();
+
+        client = TwitterApp.getRestClient();
+        client.getUserProfile(new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                User user = null;
+                try {
+                    user = User.fromJSON(response);
+                    getSupportActionBar().setTitle(user.screenName);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
