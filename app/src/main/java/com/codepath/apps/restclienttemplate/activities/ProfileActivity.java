8cd3c5ea6216949658f1fb0d428dd448c5c6c3ed
Sagar Mutha <sagar2305@gmail.com>
2017-10-07 14:52:29 -0700

    create a fragment with data using bundle

--- app/src/main/java/com/codepath/apps/restclienttemplate/activities/ProfileActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/activities/ProfileActivity.java
@@ -1,9 +1,11 @@
 package com.codepath.apps.restclienttemplate.activities;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
 import com.codepath.apps.restclienttemplate.R;
+import com.codepath.apps.restclienttemplate.fragments.UserTimelineFragment;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -11,5 +13,18 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        String screenName = getIntent().getStringExtra("screen_name");
+        //create user fragment
+        UserTimelineFragment userTimelineFragment = UserTimelineFragment.newInstance(screenName);
+
+        //display user timeline fragment
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+
+        // make change
+        ft.replace(R.id.flContainer, userTimelineFragment);
+
+        // commit transaction
+        ft.commit();
     }
 }
